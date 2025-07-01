511c310d31e39f9b0aacb94d5f8192dde0fcde31
Ninjasri98 <palitsriparno@gmail.com>
2025-07-01 10:11:37 +0530

    started building theme selector component

--- src/store/useCodeEditorStore.ts
+++ src/store/useCodeEditorStore.ts
@@ -1,4 +1,5 @@
 import { CodeEditorState } from "@/types";
+import { Monaco } from "@monaco-editor/react";
 import { create } from "zustand";
 
 const getInitialState = () => {
@@ -26,5 +27,51 @@ export const useCodeEditorStore = create<CodeEditorState>((set, get) => {
 
     const initialState = getInitialState();
 
-    return {}
+    return {
+        ...initialState,
+        output: "",
+        isRunning: false,
+        error: null,
+        editor: null,
+        executionResult: null,
+
+        getCode: () => get().editor?.getValue() || "",
+
+        setEditor: (editor: Monaco) => {
+            const savedCode = localStorage.getItem(`editor-code-${get().language}`);
+            if (savedCode) editor.setValue(savedCode);
+
+            set({ editor });
+        },
+
+        setTheme: (theme: string) => {
+            localStorage.setItem("editor-theme", theme);
+            set({ theme });
+        },
+
+        setFontSize: (fontSize: number) => {
+            localStorage.setItem("editor-font-size", fontSize.toString());
+            set({ fontSize });
+        },
+
+        setLanguage: (language: string) => {
+            const currentCode = get().editor?.getValue();
+            if (currentCode) {
+                localStorage.setItem(`editor-code-${get().language}`, currentCode);
+            }
+
+            localStorage.setItem("editor-language", language);
+
+            set({
+                language,
+                output: "",
+                error: null,
+            });
+        },
+
+        runCode : async () =>{
+            
+        }
+
+    }
 })

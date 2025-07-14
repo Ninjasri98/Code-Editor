30d59ead44c7dc65765269f9a8b2f9f45a6cb4e5
Ninjasri98 <palitsriparno@gmail.com>
2025-07-14 09:15:38 +0530

    added components for pricing page

--- src/app/(root)/_components/HeaderProfileBtn.tsx
+++ src/app/(root)/_components/HeaderProfileBtn.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import LoginButton from "@/components/LoginButton";
+import { SignedOut, UserButton } from "@clerk/nextjs";
 import { User } from "lucide-react";
 
 function HeaderProfileBtn() {
@@ -17,7 +18,7 @@ function HeaderProfileBtn() {
       </UserButton>
 
       <SignedOut>
-        <SignInButton />
+        <LoginButton />
       </SignedOut>
     </>
   );
